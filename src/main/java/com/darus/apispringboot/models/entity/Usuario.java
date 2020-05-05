0f03d952be801d76ed46258d3633666ec9237d8e
Claudio Vargas <31100054+SrDarus@users.noreply.github.com>
2020-05-05 17:02:27 -0400

    se modifica el modelo para usuario y se reemplazar y eliminar por actualizar

--- src/main/java/com/darus/apispringboot/models/entity/Usuario.java
+++ src/main/java/com/darus/apispringboot/models/entity/Usuario.java
@@ -14,27 +14,18 @@ import javax.persistence.TemporalType;
 @Entity
 @Table(name="usuario")
 public class Usuario implements Serializable{
-
-	@Id
-	@GeneratedValue
-	public String rut;
-	public String nombre;
-	public int edad;
-	public String usuario;
-	public String password;
 	
+	@Id
+	//@GeneratedValue
+	public String rut;
+	public int perfil;
+	public String nombre;
+	public String apellido;
+	public Date fechaNacimiento;
+	public String password;
 	@Column(name="fecha_creacion")
 	@Temporal(TemporalType.DATE)
 	public Date fecha;
-	public int perfil;
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
 
 	public String getRut() {
 		return rut;
@@ -44,47 +35,65 @@ public class Usuario implements Serializable{
 		this.rut = rut;
 	}
 
-	public int getEdad() {
-		return edad;
-	}
-
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
-
-	public String getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(String usuario) {
-		this.usuario = usuario;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
 	public int getPerfil() {
 		return perfil;
 	}
 
+
 	public void setPerfil(int perfil) {
 		this.perfil = perfil;
 	}
 
-	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
 	public Date getFecha() {
 		return fecha;
 	}
 
+
 	public void setFecha(Date fecha) {
-		this.fecha= fecha;
+		this.fecha = fecha;
 	}
-	
 
 	/**
 	 * 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO Usuario (rut, edad, fecha_creacion, nombre, password, perfil, usuario) VALUES ('162682946', '34', '2020-04-29', 'claudio', 'Caradej1', '1', 'darus');
-INSERT INTO Usuario (rut, edad, fecha_creacion, nombre, password, perfil, usuario) VALUES ('104444199', '55', '2020-04-29', 'margarita', 'Caradej1', '2', 'magi');
-INSERT INTO Usuario (rut, edad, fecha_creacion, nombre, password, perfil, usuario) VALUES ('164400550', '34', '2020-04-29', 'cindy', 'lenardo22', '2', 'chichi');
+INSERT INTO Usuario (rut, fecha_nacimiento, fecha_creacion, nombre, password, perfil, usuario) VALUES ('162682946', '2020-04-29', '2020-04-29', 'claudio', 'Caradej1', '1', 'darus');
+INSERT INTO Usuario (rut, fecha_nacimiento, fecha_creacion, nombre, password, perfil, usuario) VALUES ('104444199', '2020-04-29', '2020-04-29', 'margarita', 'Caradej1', '2', 'magi');
+INSERT INTO Usuario (rut, fecha_nacimiento, fecha_creacion, nombre, password, perfil, usuario) VALUES ('164400550', '2020-04-29', '2020-04-29', 'cindy', 'lenardo22', '2', 'chichi');
