213bcb90dd86eb067b05fe890a38476051f1868d
Claudio Vargas <31100054+SrDarus@users.noreply.github.com>
2020-07-03 04:29:36 -0400

    d

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
 
 
-INSERT INTO USUARIO (email, nombre, apellido, rut, fecha_creacion, fecha_nacimiento, foto, enabled, password) VALUES ('claudio@gmail.com', 'claudio', 'vargas', '162682946', '2020-01-01', '1986-09-02', 'claudio.jpg', true, '$2a$10$4mJp0OGH46PGbmTXhYt42u.Ok/Jjh8BgdhTKpGWDFYu7Agt30pzoa');
-INSERT INTO USUARIO (email, nombre, apellido, rut, fecha_creacion, fecha_nacimiento, foto, enabled, password) VALUES ('a@gmail.com', 'a', 'a', '111111111', '2020-01-01', '1986-09-02', 'a.jpg', true, '$2a$10$AFX633tcM5AnubkKlzlAOOtopmjG61b9DzbPanZgWm6Qu65wDMxdu');
+INSERT INTO USUARIO (email, nombre, apellido, rut, fecha_creacion, fecha_nacimiento, foto, enabled, password) VALUES ('claudio@gmail.com', 'claudio', 'vargas', '162682946', '2020-01-01', '1986-09-02', 'claudio.jpg', true, '$2a$10$FKMfOPra7pd1ayVKhrpXZ.oBzZ59Q1R.pp2tPiVfl.a5O8lqmmQPm');
+INSERT INTO USUARIO (email, nombre, apellido, rut, fecha_creacion, fecha_nacimiento, foto, enabled, password) VALUES ('a@gmail.com', 'a', 'a', '111111111', '2020-01-01', '1986-09-02', 'a.jpg', true, '$2a$10$6tohpuotggPiF9eyFCatu.fQ67Z5PYgX2UMiX/y8T8ZsENEN5ZJ.m');
 
 INSERT INTO ROLE (nombre) VALUES ('ROLE_ADMIN');
 INSERT INTO ROLE (nombre) VALUES ('ROLE_USER');
