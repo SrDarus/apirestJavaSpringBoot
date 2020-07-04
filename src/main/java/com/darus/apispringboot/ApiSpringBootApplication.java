781451da6ec9a95dfc73d2741e4938c7d55057f2
Claudio Vargas <31100054+SrDarus@users.noreply.github.com>
2020-07-04 03:35:38 -0400

    c

--- src/main/java/com/darus/apispringboot/ApiSpringBootApplication.java
+++ src/main/java/com/darus/apispringboot/ApiSpringBootApplication.java
@@ -7,23 +7,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class ApiSpringBootApplication implements CommandLineRunner {
+//public class ApiSpringBootApplication implements CommandLineRunner {
+	public class ApiSpringBootApplication {
 
-	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+//	@Autowired
+//	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Autowired
 	public static void main(String[] args){
 		SpringApplication.run(ApiSpringBootApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		String password= "12345";
-		for (int i = 0; i<4; i++) {
-			String passwordBScript = passwordEncoder.encode(password);
-			System.out.println("Password: "+ passwordBScript);
-		}
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//		String password= "12345";
+//		for (int i = 0; i<4; i++) {
+//			String passwordBScript = passwordEncoder.encode(password);
+//			System.out.println("Password: "+ passwordBScript);
+//		}
+//	}
 
 }
